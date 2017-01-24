ab9431eeb69f558c3b3c339b545c3ac8d4b0c671
Magnus Hagander <magnus@hagander.net>
2017-01-24 17:04:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+gitdeployer
+===========
+
+`gitdeployer` is a trivial tool to implement a git "ping hook" and
+initiate a deployment of a typical application.
+
+Configure using `gitdeployer.ini` (see sample file), don't forget to
+set up a firewall, and deploy using the systemd unit file.
+
+Security
+--------
+
+There is expected to be a firewall that prevents bad guys from calling
+the URL. There is no attempt to do rate limiting or anything like
+that.
+
+Each repository is assigned a name and a key. The url format is
+`/<reponame>/<key>/`, and any `GET` to this url will initiate a
+deployment.
+
+
+Repository types
+----------------
+
+For each repository, a type is specified. All repositories include a
+git repository root, but some types also need more information.
+
+### django
+
+This repository type is the simplest. It will just execute a `git
+fetch --rebase` in the root directory and then it will be done.
+
+A consideration for the future could be to run an automatic migration
+on it, but that's not implemented yet.
+
+### pgeustatic
+
+This repository type deploys a repository using the `deploystatic`
+method from pgeu. It starts by running `git pull --rebase`, and then
+executes `deploystatic`. It needs the following configuration
+variables set:
+
+`target` specifies the directory to deploy the website to.
+
+## pgeubranch
+
+This repository type deploys a repository using the `deploystatic`
+method from pgeu, but instead of working from a checkout of an
+individual branch, it can directly deploy a branch. It will run a `git
+fetch` to update the repository, and then immediately work from the
+branch specified. It needs the following configuration
+variables set:
+
+`target` specifies the directory to deploy the website to.
+
+`branch` specifies the branch name, usually including the remote, for
+example `origin/master`.
---
+++ gitdeployer.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+#
+# gitdeployer.py - simple script that listens for triggering http calls and
+#                  initiate git based deploys from it.
+#
+
+from flask import Flask, request, abort
+import configparser
+import netaddr
+import os
+import subprocess
+
+class ReloadingConfigParser(object):
+    def __init__(self, filename):
+        self.filename = filename
+        self._load()
+
+    def _load(self):
+        self.loadtime = os.stat(self.filename).st_mtime
+        self.parser = configparser.ConfigParser()
+        self.parser.read(self.filename)
+
+    def __getattr__(self, name):
+        return getattr(self.parser, name)
+
+    def refresh(self):
+        if os.stat(self.filename).st_mtime != self.loadtime:
+            print("Reloading configuration")
+            self._load()
+
+def run_command(reponame, *command):
+    # We don't bother with the actual output
+    s = subprocess.Popen(command, cwd=cfg.get(reponame, 'root'))
+    s.wait(10)
+    if s.returncode != 0:
+        raise Exception("Command {0} returned {1}".format(command[0], s.returncode))
+
+def git_operation(reponame, *operations):
+    run_command(reponame, '/usr/bin/git', *operations)
+
+cfg = ReloadingConfigParser('gitdeployer.ini')
+app = Flask('gitdeployer')
+
+deploystatic = cfg.get('global', 'deploystatic')
+
+@app.route("/deploy/<repository>/<key>")
+def deploy(repository, key):
+    cfg.refresh()
+
+    if not cfg.has_section(repository):
+        return "Repo not found", 404
+    for k in 'key', 'type', 'root':
+        if not cfg.has_option(repository, k):
+            print("Repository {0} is missing key {1}".format(repository, key))
+            return "Repo misconfigured", 500
+    if not cfg.get(repository, 'key') == key:
+        return "Invalid key", 401
+
+    # We only do git, so verify that the root is a dictionary
+    if not os.path.isdir("{0}/.git".format(cfg.get(repository, 'root'))):
+        print("Repository {0} has a root {1} that is not a git repository".format(
+            repository, cfg.get(repository, 'root')))
+        return "Not a git repo", 500
+
+    try:
+        if cfg.get(repository, 'type') == 'django':
+            # Basic django repository. For this repo type, we do a git pull. If something
+            # has changed, we count on the uwsgi process to reload the app.
+            # XXX: in the future, consider doing automatic migration?
+            git_operation(repository, 'pull', '--rebase')
+        elif cfg.get(repository, 'type') == 'pgeustatic':
+            # For pgeu static, we pull the git repo and then deploy from there.
+            # The correct branch has to be checked out.
+            for k in 'target', :
+                if not cfg.has_option(repository, k):
+                    print("Repository {0} is missing key {1}".format(repository, k))
+                    return "Repo misconfigured", 500
+
+            git_operation(repository, 'pull', '--rebase')
+            run_command(repository, deploystatic,
+                        cfg.get(repository, 'root'),
+                        cfg.get(repository, 'target'),
+            )
+        elif cfg.get(repository, 'type') == 'pgeubranch':
+            # For pgeu branch, we fetch the git repository and then deploy directly from
+            # that using a tar export.
+            for k in 'target', 'branch' :
+                if not cfg.has_option(repository, k):
+                    print("Repository {0} is missing key {1}".format(repository, k))
+                    return "Repo misconfigured", 500
+
+            git_operation(repository, 'fetch')
+            run_command(repository, deploystatic,
+                        cfg.get(repository, 'root'),
+                        cfg.get(repository, 'target'),
+                        '--branch',
+                        cfg.get(repository, 'branch'),
+            )
+        else:
+            print("Repository {0} has an invalid type {1}".format(
+                repository, cfg.get(repository, 'type')))
+            return "Invalid git repo type", 500
+    except Exception as e:
+        print("Failed to update {0}: {1}".format(repository, e))
+        return "Internal error", 500
+
+    print("Deployed repository {0}".format(repository))
+    return "OK"
+
+@app.before_request
+def limit_remote_addr():
+    cfg.refresh()
+    for a in cfg.get('global', 'sources').split():
+        if netaddr.IPAddress(request.remote_addr) in netaddr.IPNetwork(a):
+            return
+    abort(403)
+
+if __name__ == "__main__":
+    addresses = [netaddr.IPNetwork(c) for c in cfg.get('global', 'sources').split()]
+    print(addresses)
+
+    app.run(debug=cfg.get("global", "debug", fallback=False),
+            port=cfg.getint('global', 'port'))
